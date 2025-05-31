5acc30d36e766d832aa453e9037db8e3dd7060ad
ekeledochidi <ekeledochidiebere@gmail.com>
2025-05-31 03:35:41 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -63,9 +63,17 @@ public_users.get('/title/:title', function (req, res) {
 
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/reviews/:isbn', function (req, res) {
+    const bookId = req.params.isbn;
+
+    // Check if the book exists
+    if (books[bookId]) {
+        const reviews = books[bookId].reviews;
+        res.status(200).json({ reviews: reviews });
+    } else {
+        res.status(404).json({ message: "Book not found" });
+    }
 });
+
 
 module.exports.general = public_users;
