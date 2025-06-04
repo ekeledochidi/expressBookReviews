a3eaf5ca27f90427511f11d52a97f50665bafd6b
ekeledochidi <ekeledochidiebere@gmail.com>
2025-06-04 01:05:29 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,7 +38,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
-  
+
+// async to get books
+async function getBooks() {
+    return {books};
+}
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author.toLowerCase();
